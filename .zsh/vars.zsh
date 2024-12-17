14127242ea6626a4756595cd1f21f720a42124c2
kostabekre <kostabekre@gmail.com>
2024-12-17 12:57:33 +0300

    new changes

--- .zsh/vars.zsh
+++ .zsh/vars.zsh
@@ -1,8 +1,10 @@
 # Language, Keyboard
-export GTK_IM_MODULE=fcitx
-export QT_IM_MODULE=fcitx
-export XMODIFIERS=@im=fcitx
-export GLFW_IM_MODULE=fcitx
-export SDL_IM_MODULE=fcitx
-export QT_QPA_PLATFORM=xcb
+#export GTK_IM_MODULE=fcitx
+#export QT_IM_MODULE=fcitx
+#export XMODIFIERS=@im=fcitx
+#export GLFW_IM_MODULE=fcitx
+#export SDL_IM_MODULE=fcitx
+#export QT_QPA_PLATFORM=xcb
+
 export PATH=$PATH:$HOME/Applications
+export YDOTOOL_SOCKET="/run/.ydotool_socket"
